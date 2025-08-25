6c7c4d1b4d5471e1924f4e3deadc64d98f9eb5e3
Illia Rozhok <i_rozhok@icloud.com>
2025-08-25 23:48:16 +0300

    remove redundant auth service in google strategy

--- backend/src/modules/auth/strategies/google.strategy.ts
+++ backend/src/modules/auth/strategies/google.strategy.ts
@@ -2,13 +2,11 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy, VerifyCallback } from "passport-google-oauth20";
 import { ConfigService } from "@nestjs/config";
-import { AuthService } from "../auth.service";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
   constructor(
     private readonly configService: ConfigService,
-    private readonly authService: AuthService
   ) {
     const clientID = configService.get("GOOGLE_CLIENT_ID");
     const clientSecret = configService.get("GOOGLE_CLIENT_SECRET");
