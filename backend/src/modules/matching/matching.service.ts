fc790c3fae42f538935b409879fb9641c14d7d6e
illiacodes <79791021+IlyaRozhok@users.noreply.github.com>
2026-04-01 16:05:05 +0300

    fix matching

--- backend/src/modules/matching/matching.service.ts
+++ backend/src/modules/matching/matching.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository, SelectQueryBuilder } from "typeorm";
-import { Property } from "../../entities/property.entity";
-import { Preferences } from "../../entities/preferences.entity";
-import { User } from "../../entities/user.entity";
+import { Property } from "@/entities";
+import { Preferences } from "@/entities";
 import { MatchingCalculationService } from "./services/matching-calculation.service";
-import { S3Service } from "../../common/services/s3.service";
+import { S3Service } from "@/common/services/s3.service";
 import {
   PropertyMatchResult,
   MatchingOptions,
@@ -21,7 +20,6 @@ export class MatchingService {
     private readonly propertyRepository: Repository<Property>,
     @InjectRepository(Preferences)
     private readonly preferencesRepository: Repository<Preferences>,
-    @InjectRepository(User)
     private readonly calculationService: MatchingCalculationService,
     private readonly s3Service: S3Service
   ) {}
